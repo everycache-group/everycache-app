108c7d5d2e71562547c618f3e60ecd33a232591e
Dominik Niemiro <dniemiro@protonmail.com>
2021-10-06 20:17:31 +0200

    Revert the changes to 'everycache_api.api'

--- api/everycache_api/api/resources/user.py
+++ api/everycache_api/api/resources/user.py
@@ -122,7 +122,7 @@ class UserResource(Resource):
     def put(self, username: str):
         # ensure current_user is authorized
         if current_user.username != username and current_user.role != User.Role.Admin:
-            return {}, 403
+            return 403
 
         # find user
         user = User.query.filter_by(username=username).first_or_404()
@@ -138,10 +138,10 @@ class UserResource(Resource):
     def delete(self, username: str):
         # ensure current_user is authorized
         if current_user.username != username and current_user.role != User.Role.Admin:
-            return {}, 403
+            return 403
 
         # find user
-        user = User.query.filter_by(username=username).first_or_404()
+        user = User.query.filter(username=username).first_or_404()
 
         # mark user as deleted
         user.deleted = True
