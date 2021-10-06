cf012d042a28b3450fe40ec33be3e369ee16ae18
Dominik Niemiro <dniemiro@protonmail.com>
2021-10-06 17:45:39 +0200

    Add 'api.views.cache.CacheResource.delete' tests

--- api/everycache_api/tests/api/test_cache.py
+++ api/everycache_api/tests/api/test_cache.py
@@ -124,6 +124,21 @@ class TestCachePut:
         for key, value in _put_data_dict.items():
             assert type(value)(getattr(cache, key)) == value
 
+    @pytest.mark.parametrize("logged_in_user_role", list(User.Role))
+    def test_put_own_cache_by_deleted_user(self, logged_in_user_role, _put_data_dict,
+                                           client, logged_in_user):
+        user, *_ = logged_in_user
+        user.deleted = True
+        cache = CacheFactory(owner=user)
+
+        headers = get_headers_for_user(logged_in_user, logged_in_user_role)
+        response = self._send_put_request(
+            client, json.dumps(_put_data_dict), headers, cache.id_)
+
+        assert response.status_code == 403
+        for key, value in _put_data_dict.items():
+            assert type(value)(getattr(cache, key)) != value
+
     def test_put_other_users_cache(self, client, _put_data, logged_in_user):
         cache = CacheFactory()
         init_name = cache.name
@@ -133,3 +148,77 @@ class TestCachePut:
 
         assert response.status_code == 403
         assert cache.name == init_name
+
+    def test_put_other_users_cache_by_admin(self, client, _put_data, logged_in_user):
+        cache = CacheFactory()
+        init_name = cache.name
+        headers = get_headers_for_user(logged_in_user, User.Role.Admin)
+        response = self._send_put_request(
+            client, _put_data, headers, cache.id_)
+
+        assert response.status_code == 200
+        assert cache.name != init_name
+
+
+class TestCacheDelete:
+
+    def test_delete_unauthorized(self, client):
+        cache = CacheFactory()
+        response = client.delete(f"/api/caches/{cache.id_}")
+
+        assert response.status_code == 401
+
+    @pytest.mark.parametrize("logged_in_user_role", list(User.Role))
+    def test_delete_cache_not_found(self, logged_in_user_role, client, logged_in_user):
+        headers = get_headers_for_user(logged_in_user, logged_in_user_role)
+        response = client.delete("/api/caches/2", headers=headers)
+        assert response.status_code == 404
+
+    @pytest.mark.parametrize("logged_in_user_role", list(User.Role))
+    def test_delete_cache_deleted(self, logged_in_user_role, client, logged_in_user):
+        cache = CacheFactory(deleted=True)
+        headers = get_headers_for_user(logged_in_user, logged_in_user_role)
+        response = client.delete(f"/api/caches/{cache.id_}", headers=headers)
+        assert response.status_code == 404
+
+    def test_delete_other_users_cache(self, client, logged_in_user):
+        cache = CacheFactory()
+        headers = get_headers_for_user(logged_in_user, User.Role.Default)
+
+        response = client.delete(f"/api/caches/{cache.id_}", headers=headers)
+
+        assert response.status_code == 403
+        assert cache.deleted is False
+
+    def test_delete_other_users_cache_by_admin(self, client, logged_in_user):
+        cache = CacheFactory()
+        headers = get_headers_for_user(logged_in_user, User.Role.Admin)
+
+        response = client.delete(f"/api/caches/{cache.id_}", headers=headers)
+
+        assert response.status_code == 200
+        assert cache.deleted is True
+
+    @pytest.mark.parametrize("logged_in_user_role", list(User.Role))
+    def test_delete_own_cache(self, logged_in_user_role, client, logged_in_user):
+        user, *_ = logged_in_user
+        cache = CacheFactory(owner=user)
+        headers = get_headers_for_user(logged_in_user, logged_in_user_role)
+
+        response = client.delete(f"/api/caches/{cache.id_}", headers=headers)
+
+        assert response.status_code == 200
+        assert cache.deleted is True
+
+    @pytest.mark.parametrize("logged_in_user_role", list(User.Role))
+    def test_delete_own_cache_by_deleted_user(self, logged_in_user_role,
+                                              client, logged_in_user):
+        user, *_ = logged_in_user
+        user.deleted = True
+        cache = CacheFactory(owner=user)
+        headers = get_headers_for_user(logged_in_user, logged_in_user_role)
+
+        response = client.delete(f"/api/caches/{cache.id_}", headers=headers)
+
+        assert response.status_code == 403
+        assert cache.deleted is False
