83ad71eea1e33f4fc99a9d21ad6166b3b333b597
Yamsha <jakub.lacinski@gmail.com>
2021-10-12 14:23:59 +0200

    Fix code formatting

--- api/everycache_api/api/schemas/user.py
+++ api/everycache_api/api/schemas/user.py
@@ -1,31 +1,34 @@
 from string import ascii_letters, digits
 
-from everycache_api.extensions import db, ma
-from everycache_api.models import User
 from marshmallow import validate, validates
 from marshmallow.exceptions import ValidationError
 from marshmallow_enum import EnumField
 
+from everycache_api.extensions import db, ma
+from everycache_api.models import User
+
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     role = EnumField(User.Role)
-    password = ma.String(load_only=True, required=True,
-                         validate=validate.Length(8, 255))
+    password = ma.String(
+        load_only=True, required=True, validate=validate.Length(8, 255)
+    )
     deleted = ma.Boolean(dump_only=True)
 
     @validates("username")
     def validate_username(self, value):
         def _validate_username_three_letters(value):
             if sum((value.count(let) for let in ascii_letters if let in value)) < 3:
-                raise ValidationError(
-                    "Must contain at least three letters.")
+                raise ValidationError("Must contain at least three letters.")
 
         return validate.And(
             validate.ContainsOnly(
                 ascii_letters + "_" + digits,
-                error="Must consist only of letters, digits and '_'."),
+                error="Must consist only of letters, digits and '_'.",
+            ),
             validate.Length(5, 255),
-            _validate_username_three_letters)(value)
+            _validate_username_three_letters,
+        )(value)
 
     @validates("email")
     def validate_email(self, value):
