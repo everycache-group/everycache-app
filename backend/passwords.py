7218c6ba5faff9d6100841da4bb3071b4d56ba51
Dominik Niemiro <dniemiro@protonmail.com>
2021-11-04 19:48:34 +0100

    remove backend dir

--- backend/passwords.py
+++
@@ -1,9 +0,0 @@
-from passlib.hash import pbkdf2_sha256 as sha256
-
-
-def generate_hash(s: str) -> str:
-    return sha256.hash(s)
-
-
-def verify_password(password: str, hash_: str) -> bool:
-    return sha256.verify(password, hash_)
