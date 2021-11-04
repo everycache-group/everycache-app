11c2855cbd0c5a5369d754ea1c5398dfce0adf63
Dominik Niemiro <dniemiro@protonmail.com>
2021-11-04 20:58:47 +0100

    Add marhmallow error test case

--- api/everycache_api/tests/test_common.py
+++ api/everycache_api/tests/test_common.py
@@ -1,4 +1,6 @@
 import pytest
+from marshmallow import ValidationError
+from everycache_api.api.resources.user import UserListResource
 
 
 @pytest.mark.parametrize("endpoint", ("/api/users", "/api/caches"))
@@ -9,5 +11,18 @@ def test_request_list_empty_database(client, endpoint):
         "pages": 0,
         "next": f"{endpoint}?page=1&per_page=50",
         "prev": f"{endpoint}?page=1&per_page=50",
-        "results": []
+        "results": [],
     }
+
+
+def test_marshmallow_error(client, mocker):
+    msg = "test_marshmallow_error"
+    mocker.patch.object(
+        UserListResource,
+        "get",
+        side_effect=ValidationError(message=msg),
+    )
+
+    response = client.get("/api/users")
+    assert response.json == [msg]
+    assert response.status_code == 400
