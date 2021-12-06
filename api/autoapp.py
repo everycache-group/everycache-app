9128055a1e07df7e03075f086b705a0202eac8e0
Yamsha <jakub.lacinski@gmail.com>
2021-12-06 15:36:29 +0100

    Reformat autoapp.py

--- api/autoapp.py
+++ api/autoapp.py
@@ -1,7 +1,9 @@
-from everycache_api.app import create_app, start_extensions
 from flask_migrate import upgrade
 
+from everycache_api.app import create_app, start_extensions
+
 app = create_app()
+
 with app.app_context():
     upgrade(directory="everycache_api/migrations/")
 
