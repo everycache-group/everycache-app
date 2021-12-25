838d04323d07997c35f3e9f535fa5883b45572c1
DESKTOP-POJEDEN\Tamasa <tamasa@wp.pl>
2021-12-26 00:32:46 +0100

    app cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,13 @@
 import "./reset.css";
 import React from "react";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./components/navigation/Sidebar/Sidebar";
 import * as Page from "./pages/pagesExport";
 import ProtectedRoute from "./Routes/ProtectedRoute";
 
 function App() {
   const logged = useSelector((state) => state.auth.logged);
-
-  const history = useHistory();
-
-  //only for testing redux store
-  useEffect(() => {
-    window.onbeforeunload = function () {
-      window.location.reload(history.push("/submenu1"));
-    };
-    return () => {
-      window.onbeforeunload = null;
-    };
-  }, []);
 
   return (
     <>
