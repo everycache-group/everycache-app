b3eb8ed8766a4a05030247038980a52fd7c5c796
DESKTOP-POJEDEN\Tamasa <tamasa@wp.pl>
2022-01-03 22:43:08 +0100

    project cleaning

--- frontend/src/pages/pagesExport.js
+++ frontend/src/pages/pagesExport.js
@@ -1,6 +1,5 @@
 import AuthenticationPage from "./Authentication/Authentication";
 import HomePage from "./Home/Home";
-import SubMenu1Page from "./SubMenu1";
 import MapPage from "./Map/Map";
 
 export const Home = HomePage;
