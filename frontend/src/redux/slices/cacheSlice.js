2836e1591abcfcc3168ae115cb30f3e05febecdd
DESKTOP-POJEDEN\Tamasa <tamasa@wp.pl>
2022-02-03 23:21:37 +0100

    added cache CRUD

--- frontend/src/redux/slices/cacheSlice.js
+++ frontend/src/redux/slices/cacheSlice.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import config from "./../../api/api-config.json";
 import ResourceConnector from "../../services/resourceService";
 import { PrepareDataSourceTable } from "../../services/dataSourceMapperService";
+import { create } from "@mui/material/styles/createTransitions";
 
 const cache = new ResourceConnector(config.resources.cache);
 
@@ -25,6 +26,45 @@ export const getCaches = createAsyncThunk(
     };
 
     return Promise.resolve(payload);
+  }
+);
+
+export const createCache = createAsyncThunk(
+  "cache/createCache",
+  async (cacheData, { dispatch }) => {
+    try {
+      const response = await cache.create(cacheData);
+
+      return Promise.resolve(response.data);
+    } catch (_error) {
+      return Promise.reject();
+    }
+  }
+);
+
+export const updateCache = createAsyncThunk(
+  "cache/updateCache",
+  async ({ id, ...cacheData }, thunkAPI) => {
+    try {
+      const response = await cache.update(id, cacheData);
+
+      return Promise.resolve();
+    } catch (_error) {
+      return Promise.reject();
+    }
+  }
+);
+
+export const deleteCache = createAsyncThunk(
+  "cache/deleteCache",
+  async ({ id }, thunkAPI) => {
+    try {
+      const response = await cache.remove(id);
+
+      return Promise.resolve(id);
+    } catch (error) {
+      return Promise.reject(id);
+    }
   }
 );
 
@@ -65,6 +105,22 @@ const cacheSlice = createSlice({
     },
     [getCaches.pending]: (state, action) => {
       state.loading = true;
+    },
+
+    [createCache.fulfilled]: (state, action) => {
+      state.caches.push(action.payload);
+    },
+    [updateCache.fulfilled]: (state, action) => {
+      const index = state.caches.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.caches[index] = action.payload;
+    },
+    [deleteCache.fulfilled]: (state, action) => {
+      const index = state.caches.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.caches = state.caches.splice(index, 1);
     },
   },
 });
